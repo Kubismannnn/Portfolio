e08dbad905e292c4161f0479ad56f85d5f3ee463
Jakub Líska <liskajakub612@outlook.com>
2025-05-05 02:32:08 +0200

    Workin on setting favicon and <head> things and app.vue site

--- app.vue
+++ app.vue
@@ -1,9 +1,10 @@
-<template lang="html">
-
+<template>
+<!--I dont need to specify language i write in, template is allways for HTML-->
+<h1>Jakub Líska</h1>
 
 </template>
 
 <style>
-
+/*Same is here in style, style is for CSS */
 
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+export default {
+  head:{
+    title: "Jakub Líska Portfolio",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "description", content: "Jakub Líska Portfolio" },
+      { name: "keywords", content: "Jakub Líska, portfolio, web developer" },
+      { name: "author", content: "Jakub Líska" },
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "img/Moje Fotka.jpeg" },
+    ],
+  }
+}
